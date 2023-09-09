9d18e0e26c6066a963aa59c2ddf02bcbfa08035e
Shayne <shngli@users.noreply.github.com>
2023-09-09 20:53:16 +0800

    Update main.py
    update code comments

--- Text-Embedding/main.py
+++ Text-Embedding/main.py
@@ -1,3 +1,7 @@
+"""
+Provide text embedding about unicorn startups to generate context for OpenAI 
+about startup company from the user input (eg. Tell me about company [blank] and who invest in it)
+"""
 import openai 
 import numpy as np
 import pandas as pd
@@ -5,15 +9,16 @@ openai.api_key = os.getenv("OPENAI_API_KEY")
 
 df = pd.read_csv('./data/unicorns_with_embeddings.csv')
 
+### Note this function assumes we already set our Open AI key
 def get_embedding(text):
-    ### Note how this function assumes we already set our Open AI key!
+    
     result = openai.Embedding.create(
       model='text-embedding-ada-002',
       input=text
     )
     return result["data"][0]["embedding"]
 
-
+### Note: There are vector search services for larger amount of vectors eg. Pinecone or Weaviate
 def vector_similarity(vec1,vec2):
     """
     Returns the similarity between two vectors.
@@ -23,17 +28,20 @@ def vector_similarity(vec1,vec2):
     return np.dot(np.array(vec1), np.array(vec2))
 
 def embed_prompt_lookup():
-    ### initial question
+    ### Input initial user question
     question = input("What question do you have about a Unicorn company? ")
-    ### Get embedding
+    
+    ### Create prompt embedding from the user input
     prompt_embedding = get_embedding(question)
-    ### Get prompt similarity with embeddings
+    
+    ### Get prompt similarity with embeddings from the startup summaries
     ### Note how this will overwrite the prompt similarity column each time!
     df["prompt_similarity"] = df['embedding'].apply(lambda vector: vector_similarity(vector, prompt_embedding))
 
-    ### get most similar summary
+    ### Get most similar summary to the prompt embedding
     summary = df.nlargest(1,'prompt_similarity').iloc[0]['summary'] 
 
+    ### Submit context about startup company in user prompt to OpenAI Completion API
     prompt = f"""Only answer the question below if you have 100% certainty of the facts, use the context below to answer.
             Here is some context:
             {summary}
@@ -46,6 +54,8 @@ def embed_prompt_lookup():
         max_tokens=500,
         model="text-davinci-003"
     )
+    
+    ### Print OpenAI response about the startup company that user asked about
     print(response["choices"][0]["text"].strip(" \n"))
 
 if __name__ == "__main__":
